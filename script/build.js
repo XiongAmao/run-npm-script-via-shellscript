cbeab420b0928d85260120a719fef0272d292928
XiongAmao <onycat@live.cn>
2021-04-21 11:01:16 +0800

    add std log

--- script/build.js
+++ script/build.js
@@ -9,11 +9,18 @@ const cmdMap = {
 const cmd = cmdMap[env]
 
 if (cmd) {
-  exec(cmd, (err, stdout, stderr) => {
+  const child = exec(cmd, (err, stdout, stderr) => {
     if (err) {
       console.error(err)
     }
     console.log(stdout)
     console.error(stderr)
+  })
+
+  child.stdout.on('data', (data) => {
+    console.log(data)
+  })
+  child.stderr.on('data', (data) => {
+    console.error(data)
   })
 }
